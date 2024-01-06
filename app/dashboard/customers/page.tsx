58f9601fbdf4046ab1abf4b31c9826d5e0d0059d
Chris Seaman <christopherseaman1983@gmail.com>
2024-01-06 13:32:37 +0000

    update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,31 @@
+import { Metadata } from "next"
 
-export default function CustomersPage() {
+import { fetchCustomers, fetchInvoiceById, fetchFilteredCustomers } from "@/app/lib/data"
+
+export const metadata : Metadata = {
+  title: "customers"
+}
+
+// export type CustomersTableType = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: number;
+//   total_paid: number;
+// };
+
+export default async function page() {
+
+  const getCustomers = fetchCustomers()
+  const customers = await getCustomers
+
+  
+
+
+
   return (
-    <div>Customers Page</div>
+    <div>Cutomers</div>
   )
 }
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,32 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+
+import { Metadata } from 'next';
+
+export async function generateMetadata({params} : {params : {id : string}}) : Promise<Metadata>{
+
+  const {id} = params
+
+  const getInvoice = fetchInvoiceById(id)
+  const invoice = await getInvoice
+
+  const getCustomers = fetchCustomers()
+  const customers = await getCustomers
+  const customer = customers.find( person => person.id === invoice.customer_id)
+  
+  if(!invoice || !customer){
+    return {
+      title: "Unknown Customer"
+    }
+  }
+
+  return {
+    title: `${customer.name} ${invoice.id}`
+  }
+}
+
+
 export default async function Page({params} : {params : {id: string}}) {
   const {id} = params
   const [invoice, customers] = await Promise.all([
